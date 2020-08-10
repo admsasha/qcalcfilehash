d01541e47a87fb1a37a57b3f41100680cb8a3272
sasha <dik@inbox.ru>
2020-08-10 15:51:26 +0900

    update locales

--- langs/qcalcfilehash_en.ts
+++ langs/qcalcfilehash_en.ts
@@ -4,38 +4,7 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
-        <source>MainWindow</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="27"/>
-        <source>Close</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="43"/>
-        <location filename="mainwindow.ui" line="212"/>
-        <source>TextLabel</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="59"/>
-        <source>Open</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="87"/>
-        <source>Algorithm:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="119"/>
-        <source>Calc</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="135"/>
+        <location filename="../mainwindow.ui" line="79"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
@@ -44,42 +13,62 @@ p, li { white-space: pre-wrap; }
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="163"/>
+        <location filename="../mainwindow.ui" line="172"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="33"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="46"/>
+        <source>Algorithm:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="109"/>
+        <source>Calc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="26"/>
         <source>Path:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="176"/>
+        <location filename="../mainwindow.ui" line="139"/>
         <source>Copyright DanSoft. All rights reserved.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="189"/>
+        <location filename="../mainwindow.ui" line="119"/>
         <source>Check:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="225"/>
+        <location filename="../mainwindow.ui" line="162"/>
         <source>Compare the hash</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="52"/>
+        <location filename="../mainwindow.cpp" line="76"/>
         <source>Open file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="52"/>
+        <location filename="../mainwindow.cpp" line="76"/>
         <source>All Files (*)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="116"/>
+        <location filename="../mainwindow.cpp" line="132"/>
         <source>equally</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="118"/>
+        <location filename="../mainwindow.cpp" line="134"/>
         <source>different</source>
         <translation type="unfinished"></translation>
     </message>
@@ -87,38 +76,38 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>ObjectConsole</name>
     <message>
-        <location filename="ObjectConsole.cpp" line="22"/>
+        <location filename="../ObjectConsole.cpp" line="22"/>
         <source>Invalid positional arguments</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ObjectConsole.cpp" line="53"/>
+        <location filename="../ObjectConsole.cpp" line="53"/>
         <source>Invalid hash argument</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ObjectConsole.cpp" line="57"/>
+        <location filename="../ObjectConsole.cpp" line="57"/>
         <source>Filename: %s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ObjectConsole.cpp" line="87"/>
+        <location filename="../ObjectConsole.cpp" line="87"/>
         <source>Result: %s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ObjectConsole.cpp" line="91"/>
-        <location filename="ObjectConsole.cpp" line="93"/>
+        <location filename="../ObjectConsole.cpp" line="91"/>
+        <location filename="../ObjectConsole.cpp" line="93"/>
         <source>Check: %s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ObjectConsole.cpp" line="91"/>
+        <location filename="../ObjectConsole.cpp" line="91"/>
         <source>equally</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ObjectConsole.cpp" line="93"/>
+        <location filename="../ObjectConsole.cpp" line="93"/>
         <source>different</source>
         <translation type="unfinished"></translation>
     </message>
@@ -126,36 +115,41 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>QCoreApplication</name>
     <message>
-        <location filename="main.cpp" line="53"/>
+        <location filename="../main.cpp" line="54"/>
         <source>Source file to hash</source>
         <oldsource>Source file to hash.</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="54"/>
+        <location filename="../main.cpp" line="55"/>
         <source>Hash algorithm</source>
         <oldsource>Hash algorithm.</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="57"/>
+        <location filename="../main.cpp" line="58"/>
         <source>hash comparison</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="58"/>
+        <location filename="../main.cpp" line="59"/>
         <source>hash</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="62"/>
+        <location filename="../main.cpp" line="63"/>
         <source>Show progress</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="65"/>
+        <location filename="../main.cpp" line="66"/>
         <source>Show list all hash algorithm</source>
         <oldsource>Show list all hash algorithm.</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>Open in gui</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- langs/qcalcfilehash_ru.ts
+++ langs/qcalcfilehash_ru.ts
@@ -4,87 +4,71 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
-        <source>MainWindow</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="27"/>
-        <source>Close</source>
-        <translation>Выход</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="43"/>
-        <location filename="mainwindow.ui" line="212"/>
-        <source>TextLabel</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="59"/>
-        <source>Open</source>
-        <translation>Открыть</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="87"/>
-        <source>Algorithm:</source>
-        <translation>Алгоритм:</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="119"/>
-        <source>Calc</source>
-        <translation>Посчитать</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="135"/>
+        <location filename="../mainwindow.ui" line="79"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <oldsource>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
-&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
-p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</oldsource>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="163"/>
+        <location filename="../mainwindow.ui" line="172"/>
+        <source>Close</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="33"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="46"/>
+        <source>Algorithm:</source>
+        <translation>Алгоритм:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="109"/>
+        <source>Calc</source>
+        <translation>Посчитать</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="26"/>
         <source>Path:</source>
         <translation>Путь:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="176"/>
+        <location filename="../mainwindow.ui" line="139"/>
         <source>Copyright DanSoft. All rights reserved.</source>
         <translation>Авторские права принадлежат DanSoft. Все права защищены.</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="189"/>
+        <location filename="../mainwindow.ui" line="119"/>
         <source>Check:</source>
         <translation>Проверка:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="225"/>
+        <location filename="../mainwindow.ui" line="162"/>
         <source>Compare the hash</source>
         <translation>Сравнение с хешем</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="52"/>
+        <location filename="../mainwindow.cpp" line="76"/>
         <source>Open file</source>
         <translation>Открыть файл</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="52"/>
+        <location filename="../mainwindow.cpp" line="76"/>
         <source>All Files (*)</source>
         <translation>Все файлы (*)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="116"/>
+        <location filename="../mainwindow.cpp" line="132"/>
         <source>equally</source>
         <translation>одинаковый</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="118"/>
+        <location filename="../mainwindow.cpp" line="134"/>
         <source>different</source>
         <translation>разный</translation>
     </message>
@@ -96,17 +80,17 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>ObjectConsole</name>
     <message>
-        <location filename="ObjectConsole.cpp" line="22"/>
+        <location filename="../ObjectConsole.cpp" line="22"/>
         <source>Invalid positional arguments</source>
         <translation>Не верно указаны аргументы</translation>
     </message>
     <message>
-        <location filename="ObjectConsole.cpp" line="53"/>
+        <location filename="../ObjectConsole.cpp" line="53"/>
         <source>Invalid hash argument</source>
         <translation>Указан несуществующий hash алгоритм</translation>
     </message>
     <message>
-        <location filename="ObjectConsole.cpp" line="57"/>
+        <location filename="../ObjectConsole.cpp" line="57"/>
         <source>Filename: %s</source>
         <translation>Имя файла: %s</translation>
     </message>
@@ -115,23 +99,23 @@ p, li { white-space: pre-wrap; }
         <translation type="vanished">Результат: </translation>
     </message>
     <message>
-        <location filename="ObjectConsole.cpp" line="91"/>
-        <location filename="ObjectConsole.cpp" line="93"/>
+        <location filename="../ObjectConsole.cpp" line="91"/>
+        <location filename="../ObjectConsole.cpp" line="93"/>
         <source>Check: %s</source>
         <translation>Проверка: %s</translation>
     </message>
     <message>
-        <location filename="ObjectConsole.cpp" line="91"/>
+        <location filename="../ObjectConsole.cpp" line="91"/>
         <source>equally</source>
         <translation>одинаковый</translation>
     </message>
     <message>
-        <location filename="ObjectConsole.cpp" line="93"/>
+        <location filename="../ObjectConsole.cpp" line="93"/>
         <source>different</source>
         <translation>разный</translation>
     </message>
     <message>
-        <location filename="ObjectConsole.cpp" line="87"/>
+        <location filename="../ObjectConsole.cpp" line="87"/>
         <source>Result: %s</source>
         <translation>Результат: %s</translation>
     </message>
@@ -139,37 +123,42 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>QCoreApplication</name>
     <message>
-        <location filename="main.cpp" line="53"/>
+        <location filename="../main.cpp" line="54"/>
         <source>Source file to hash</source>
         <oldsource>Source file to hash.</oldsource>
         <translation>Исходный файл для хеширования</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="54"/>
+        <location filename="../main.cpp" line="55"/>
         <source>Hash algorithm</source>
         <oldsource>Hash algorithm.</oldsource>
         <translation>Хеш-алгоритм</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="57"/>
+        <location filename="../main.cpp" line="58"/>
         <source>hash comparison</source>
         <translation>Хеш для сравнения</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="58"/>
+        <location filename="../main.cpp" line="59"/>
         <source>hash</source>
         <translation>Хеш</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="62"/>
+        <location filename="../main.cpp" line="63"/>
         <source>Show progress</source>
         <translation>Показывать процесс</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="65"/>
+        <location filename="../main.cpp" line="66"/>
         <source>Show list all hash algorithm</source>
         <oldsource>Show list all hash algorithm.</oldsource>
         <translation>Вывести все хеш-алгоритмы</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>Open in gui</source>
+        <translation>Открыть в GUI</translation>
     </message>
     <message>
         <source>Result: </source>
