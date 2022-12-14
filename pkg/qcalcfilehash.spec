6d1deb437f5aee48659bb1e92195e7755c2cdd8c
admsasha <dik@inbox.ru>
2022-12-14 10:33:47 +0900

    release version 1.0.8

--- pkg/qcalcfilehash.spec
+++ pkg/qcalcfilehash.spec
@@ -1,6 +1,6 @@
 Summary:	Hash calculator
 Name:		qcalcfilehash
-Version:	1.0.7
+Version:	1.0.8
 Release:	1
 License:	GPLv3+
 Group:		File tools
--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "1.0.7"
-#define DATE_BUILD "12.08.2020"
+#define VERSION "1.0.8"
+#define DATE_BUILD "14.12.2022"
 
 #endif // VERSION_H
